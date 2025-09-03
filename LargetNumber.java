31bb28a9769bc9d8b586b4658d0e0064158c9e4b
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-03 12:29:31 +0530

    Add LargetNumber.java: find largest and smallest number in array

---
+++ LargetNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class LargetNumber {
+    public static int findLargest(int[] numbers) {
+        int largest = Integer.MIN_VALUE; // -Infinity
+        int smallest = Integer.MAX_VALUE; // +Infinity
+        for (int i = 0; i < numbers.length; i++) {
+            if (largest < numbers[i]) {
+                largest = numbers[i];
+            }
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("The smallest number is: " + smallest);
+        return largest;
+
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 3, 5, 7, 2, 8, 20, 56, 44, 4, 3 };
+        int largest = findLargest(numbers);
+        System.out.println("The largest number is: " + largest);
+    }
+}
