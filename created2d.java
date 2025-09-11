ad80f4c2e8432c3d3fdaf158998648584f6c9237
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-11 10:52:24 +0530

    creat 2d aray and searching number

---
+++ created2d.java
@@ -0,0 +1,33 @@
+
+public class created2d {
+    public static void serching(int[][] matrix, int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("Element found at: (" + i + ", " + j + ")");
+                    return;
+                }
+            }
+        }
+        System.out.println("Element not found");
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+        // Print the 2D array
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int key = 5;
+        serching(matrix, key);
+    }
+
+}
