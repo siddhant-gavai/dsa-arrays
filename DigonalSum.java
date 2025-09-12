c788155e623e34d4306a218816bf8fb1e962e1ad
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-12 15:42:08 +0530

    Optimal solution for Diagonal sum

--- DigonalSum.java
+++ DigonalSum.java
@@ -9,15 +9,25 @@ public class DigonalSum {
 
         int sum = 0;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (i == j) {
-                    sum = sum + matrix[i][j];
-                } else if (i + j == matrix.length - 1) {
-                    sum += matrix[i][j];
-                }
-            }
+        // for (int i = 0; i < matrix.length; i++) {
+        // for (int j = 0; j < matrix[0].length; j++) {
+        // if (i == j) {
+        // sum = sum + matrix[i][j];
+        // } else if (i + j == matrix.length - 1) {
+        // sum += matrix[i][j];
+        // }
+        // }
 
+        // }
+        // System.out.println("the sum is " + sum);
+
+        for (int i = 0; i < matrix.length; i++) {
+            // Primary diagonal
+            sum = sum + matrix[i][i];
+            // Secondary Diagonal
+            if (i != matrix.length - 1 - i) {
+                sum = sum + matrix[i][matrix.length - i - 1];
+            }
         }
         System.out.println("the sum is " + sum);
 
